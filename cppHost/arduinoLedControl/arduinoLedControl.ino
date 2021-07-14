3632408291eed294cd4f4a13b0457b1498d94ae5
Lucas Lelièvre <lucas.lelievre98@gmail.com>
2021-07-14 21:59:35 +0200

    update arduino code to use FastLED library

--- cppHost/arduinoLedControl/arduinoLedControl.ino
+++ cppHost/arduinoLedControl/arduinoLedControl.ino
@@ -1,51 +1,45 @@
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
+//#include <Adafruit_NeoPixel.h>
 
 #define PIN 6
 #define NUM_LED 60
 #define NUM_DATA 182
 #define RECON_TIME 1000
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LED, PIN, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LED, PIN, NEO_GRB + NEO_KHZ800);
 
-uint8_t led_color[NUM_DATA];
+uint8_t data[NUM_DATA];
+CRGB leds[NUM_LED];
 int index = 0;
-unsigned long before = 0;
-unsigned long now = 0;
 
 void setup() {
-  strip.begin();
-  strip.show();
   Serial.begin(115200);
+
+  //strip.begin();
+  //strip.show();
+  FastLED.addLeds<WS2812, PIN, GRB>(leds, NUM_LED).setCorrection(TypicalLEDStrip);
+  FastLED.clear();
+  FastLED.show();
+
   Serial.print("ozy");
 }
 
 void loop() {
-
-
   if (Serial.available() > 0) {
-    led_color[index++] = (uint8_t)Serial.read();
+    data[index++] = (uint8_t)Serial.read();
 
     if (index > NUM_DATA) {
       index = 0;
-      if ((led_color[0] == 'o') && (led_color[1] == 'z')) {
+      if ((data[0] == 'o') && (data[1] == 'z')) {
         for (int i = 0; i < NUM_LED; i++) {
           int led_index = i * 3 + 2;
-          strip.setPixelColor(i, strip.Color(led_color[led_index], led_color[led_index + 1], led_color[led_index + 2]));
+          //strip.setPixelColor(i, strip.Color(data[led_index], data[led_index + 1], data[led_index + 2]));
+          leds[i].setRGB((int) data[led_index], (int) data[led_index] + 1, (int) data[led_index] + 2);
         }
-        strip.show();
+        //strip.show();
+        FastLED.show();
       }
     }
     Serial.print('y');
   }
-
-  /*
-    before = millis();
-    // max 3ms
-    for (int i = 0; i < NUM_LED; i++){
-      strip.setPixelColor(i, 255, 255, 255);
-    }
-    strip.show();
-
-    Serial.println(millis() - before);
-  */
 }
